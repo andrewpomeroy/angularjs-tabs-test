f85ae244c8d7d9f791d8b1831984ddd02fac985a
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2020-03-16 13:36:01 -0700

    watch

--- src/components/tab.js
+++ src/components/tab.js
@@ -1,6 +1,5 @@
 import angular from "angular";
 import template from "./tab.html";
-import uuid from "uuid4";
 
 export default {
 	bindings: {
@@ -17,23 +16,8 @@ export default {
 function TabController() {
 	var $ctrl = this;
 
-	var setupWatchers = function () {
-		// if ($ctrl.watch) {
-		// 	var watchList;
-		// 	if (!Array.isArray($ctrl.watch)) {
-		// 		watchList = [];
-		// 		watchList.push($ctrl.watch);
-		// 	}
-		// 	else {
-				
-		// 	}
-		// }
-	};
-
 	$ctrl.$postLink = function () {
-		$ctrl.uuid = uuid();
 		$ctrl.tabs.registerTab(this);
-		console.log("hello", $ctrl.tabs.registerTab);
 	};
 
 	$ctrl.$onDestroy = function () {
@@ -41,7 +25,9 @@ function TabController() {
 	};
 
 	$ctrl.$onChanges = function (changes) {
-		$ctrl.tabs.updateTab(this);
+		if (changes.watch) {
+			$ctrl.tabs.updateTab(this);
+		}
 	};
 
 }
